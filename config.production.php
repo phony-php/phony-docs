8d641de5c682a7d6ded6a28b2f1b3ed7647021f6
Yunus Emre Deligöz <ye@deligoz.me>
2020-04-10 23:57:14 +0300

    Add Algolia config

--- config.production.php
+++ config.production.php
@@ -5,6 +5,6 @@ return [
     'production' => true,
 
     // DocSearch credentials
-    'docsearchApiKey' => '',
-    'docsearchIndexName' => '',
+    'docsearchApiKey' => 'fdf25c5e7da9d0b64a39ef5733cb6b88',
+    'docsearchIndexName' => 'deligoz_phony',
 ];
